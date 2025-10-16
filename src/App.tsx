85e9500c4d218973c1c42bef285d365dc77d6a9b
Marco Huertas <marcohuertas3019@gmail.com>
2025-10-15 20:55:05 -0600

    Funcionalidad de Login

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,40 @@ import './App.css'
 import { AppBarMenu } from './componets/layout/AppBarMenu'
 import { useState } from 'react'
 import { SideNav } from './componets/layout/SideNav';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { LoginForm } from './componets/auth/LoginForm';
+import Swal from 'sweetalert2';
 
 function App() {
 
   const [drawerOpen, setDrawerOpen] = useState(false);
   const handleDrawerToggle = () => setDrawerOpen(!drawerOpen);
+
   const handleLogout = () => {
-    console.log('Sesion Cerrada');
+    Swal.fire({
+      title: "Logout",
+      text: "Estas seguro de cerrar sesion?",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Si!"
+    }).then((result) => {
+      if(result.isConfirmed) {
+        window.location.href = '/login';
+      }
+    });
   }
 
   return (
-    <>
-      <CssBaseline/>
+    <Router>
+      <CssBaseline />
       <AppBarMenu onMenuClick={handleDrawerToggle} onLogout={handleLogout}></AppBarMenu>
       <SideNav open={drawerOpen} onClose={handleDrawerToggle}></SideNav>
-    </>
+      <Routes>
+        <Route path='/login' element={<LoginForm onLoginSuccess={() => window.location.href = 'www.google.com.gt'} />} />
+      </Routes>
+    </Router>
   )
 }
-
 export default App
